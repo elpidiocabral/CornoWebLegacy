dd5dfe8c3fd0f00548a8156c2c5f5ef5f3f4af6c
unknown <netoelpidio1@gmail.com>
2024-08-20 21:28:51 -0300

    Upload - Arquivos do Projeto Antigo

---
+++ js/login.js
@@ -0,0 +1,3 @@
+login_name=new Array("@wescley_gostoso-0","@duda_bts","@marcelo_opressor51","@D_master","@daniel_lima","@fulano");
+
+senha_name=new Array("menoszero","bts123","lulaladrao","fracassado","daniellima","fulano");
